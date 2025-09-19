6801888995084123ee2cfa5c3e826805eb5f623a
Roman302005 <your-email@example.com>
2025-09-19 19:03:25 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -3,10 +3,31 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/web')
 def start():
     return """<!doctype html>
     <html>
         <body>
             <h1>web-сервер на flask</h1>
-            </body>
+            <a href="/author">author</a>
+        </body>
     </html>"""
+
+
+@app.route('/author')
+def author():
+    name = 'Лелюх Роман Вячеславович'
+    group = 'ФБИ-34'
+    faculty = 'ФБ'
+
+    return """
+    <!doctype html>
+    <html>
+        <body>
+            <p>Студент: """ + name + """ </p>
+            <p>Группа: """ + group + """ </p>
+            <p>Факультет: """ + faculty + """ </p>
+            <a href="/web">web</a>
+        </body>
+    </html>
+    """
