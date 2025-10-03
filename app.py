5028d433319eddcfb85f889d5edb28bfb2c12530
Roman302005 <your-email@example.com>
2025-10-03 20:12:42 +0700

    Имя студента

--- app.py
+++ app.py
@@ -735,5 +735,6 @@ def add_flower(name):
 '''
 
 @app.route('/lab2/example')
-def example():
-    return render_template('example.html')
+def examplee():
+    name = 'Лелюх Роман'
+    return render_template('example.html', name=name)
