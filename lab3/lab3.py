073749b72b0234abad34f0f70bc542f2f78d74ba
Your Name <you@example.com>
2025-11-01 00:57:27 +0700

    Рефакторинг: static, templates

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -7,10 +7,11 @@ request_log = deque(maxlen=20)
 lab3 = Blueprint('lab3', __name__)
 
 @lab3.route('/lab3/')
-def lab():
-    name = request.cookies.get('name')
+def labbbb():
+    name = request.cookies.get('name', 'Аноним')
     name_color = request.cookies.get('name_color')
-    return render_template('lab3/lab3.html', name = name, name_color=name_color)
+    age = request.cookies.get('age', 'не указан')
+    return render_template('lab3/lab3.html', name=name, name_color=name_color, age=age)
 
 
 
